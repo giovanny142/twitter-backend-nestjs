68102b8b32b532077c6e860a9cb749535070553c
Giovanny <givoanny.lima2000@hotmail.com>
2023-05-08 14:33:37 -0300

    all endpoints for user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Put, Delete, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get('/@:username')
+    getUserByUsername(@Param() param): string {
+        return `details of username = ${param.username}`;
+    }
+
+    @Get('/:userid')
+    getUserByUserId(@Param() param): string {
+        return `details of user id = ${param.userid}`;
+    }
+
+    @Post('/')
+    createNewUser(): string {
+        return 'new user created';
+    }
+
+    @Patch('/:userid')
+    updateUserDetails(@Param() param): string {
+        return `details of user (id = ${param.userid}) updated`;
+    }
+
+    @Put(':userid/follow')
+    followUser(): string {
+        return 'you followed user';
+    }
+
+    @Delete('/:userid/follow')
+    unfollowUser(): string {
+        return 'you unfollowed user';
+    }
+
+    @Get('/:userid/followers')
+    getFollowersOfUser(): string {
+        return 'get followers of user'
+    }
+
+    @Put('/:userid/followees')
+    getFolloweesOfUser(): string {
+        return 'get followees of giver user'
+    }
+
+}
