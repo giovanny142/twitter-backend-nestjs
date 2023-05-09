deb5bcd2014ee16fce1ab22d872fd34b3fb7535d
Giovanny <givoanny.lima2000@hotmail.com>
2023-05-09 12:45:51 -0300

    fix error of package-lock.json

---
+++ src/posts/posts.entity.ts
@@ -0,0 +1,8 @@
+// import { GvzBaseEntity } from 'src/commons/base.entity';
+// import { Entity } from 'typeorm';
+
+// @Entity('posts')
+// export class PostEntity extends GvzBaseEntity [
+
+//     text: string
+// ]
