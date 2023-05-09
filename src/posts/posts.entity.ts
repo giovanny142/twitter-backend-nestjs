4c71267c155f454c8416e45abc3330151a3a2d15
Giovanny <givoanny.lima2000@hotmail.com>
2023-05-09 13:37:55 -0300

    add posts entity

--- src/posts/posts.entity.ts
+++ src/posts/posts.entity.ts
@@ -1,8 +1,40 @@
 import { GvzBaseEntity } from 'src/commons/base.entity';
-import { Entity } from 'typeorm';
+import { Entity, Column, OneToOne, JoinColumn } from 'typeorm';
 
 @Entity('posts')
 export class PostEntity extends GvzBaseEntity {
-    
-    // text: string
+
+    @Column({ length: 240, nullable: true })
+    text: string
+
+    @Column('json', { default: [] })
+    images: Array<string>
+
+    @Column({ name: 'like_count', default: 0 })
+    likeCount: number
+
+    @Column({ name: 'repost_count', default: 0 })
+    repostCount: number
+
+    @Column('json', { default: [] })
+    hashtags: Array<string>
+
+    @Column('json', { default: [] })
+    mentions: Array<Mention>
+
+    @OneToOne(() => PostEntity)
+    @JoinColumn({ name: 'orig_post_id' })
+    origPost: PostEntity
+
+    @OneToOne(() => PostEntity)
+    @JoinColumn({ name: 'reply_post_id' })
+    replyPost: PostEntity
+
+
+
+}
+
+class Mention {
+    name: string
+    id: string
 }
